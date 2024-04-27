91b6bba5f8095cd08a77c48740a817ee43ba5051
Shardul-khopkar <80389421+Shardul-khopkar@users.noreply.github.com>
2024-04-27 15:58:41 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,62 +1,29 @@
-var noseX=0;
-var noseY=0;
-var difference=0;
-var rightWristX=0;
-var leftWristX=0;
-
-function setup()
-{
-     video=createCapture(VIDEO);
-     video.size(550,500);
-     video.position(60,150);
-     
-
-     canvas=createCanvas(550,550);
-     canvas.position(760,150);
-     
-     poseNet=ml5.poseNet(video, modelLoaded);
-     poseNet.on('pose',gotPoses)
+leftWristX=0;
+rightWristX=0;
+difference=0;
+function setup() {
+    video = createCapture(VIDEO);
+    video.size(550,500);
+    video.position(100,150);
+    canvas = createCanvas(550,550);
+    canvas.position(760,150);
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose',gotPoses);
 }
-
-function modelLoaded()
-{
-    console.log('poseNet is Initialized');
+function draw() {
+    background('#8B0000');
+    textSize(difference);
+    fill('#FFE787');
+    text('Hello world', 50 ,400);
 }
-function gotPoses(results)
-{
-    if(results.length>0)
-    {
+function modelLoaded() {
+    console.log('PoseNet is Initialized by shardul');
+}
+function gotPoses(results) {
+    if (results.length>0) {
         console.log(results);
-        noseX=results[0].pose.nose.x;
-        noseY=results[0].pose.nose.y;
-        console.log("noseX="+noseX+"noseY="+noseY);
-        
-        leftWristX=results[0].pose.leftWrist.x;
-        rightWristX=results[0].pose.rightWrist.x;
-        difference=floor(leftWristX-rightWristX);
-        console.log("noseX="+noseX+"noseY="+noseY+"difference="+difference);
+        leftWristX = results[0].pose.leftWrist.x;
+        rightWristX = results[0].pose.rightWrist.x;
+        difference = leftWristX - rightWristX;
     }
 }
-function draw()
-{
-    background('#800000');
-    fill("blue");
-    stroke("yellow")
-    square(noseX,noseY,difference);
-}
-
-// Get the elements where you want to display the height, width, and position
-const note = document.querySelector('.note');
-const squareSides = document.getElementById('.square_sides');
-
-// Assume that you have a square element with id 'square'
-const square = document.getElementById('square');
-
-// Get the height, width, and position of the square
-const height = square.offsetHeight;
-const width = square.offsetWidth;
-const position = square.getBoundingClientRect();
-
-// Display the height, width, and position in the HTML page
-note.innerHTML = `Height: ${height}px<br>Width: ${width}px`;
-squareSides.innerHTML = `Top: ${position.top}px<br>Left: ${position.left}px`;
